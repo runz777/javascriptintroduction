ea153d599beb61d55d48751c05e861a5ae6cec33
raul_dev <272271915+runz777@users.noreply.github.com>
2026-07-01 22:12:45 +0200

    second update

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Hello, World!");
+
+let name = "Raul";
+let edadd = 29;
+let ciudad = "Madrid";
+
+console.log("Mi nombre es " + name + " y tengo " + edadd + " años. " + "Vivo en " + ciudad + ".");
+
+let edad = 17;
+
+console.log(edad >= 18);
+console.log(edad === 17);
+console.log(edad < 20);
+
+console.log(3 + 2) - 76 * (1 + 1)
+
+
+//Variables and Operators// 
+console.log(23 + 97)
+console.log(45 + 5 - 7 + 4 - 5 + 93)
+console.log((4 + 6 + 9) / 77)
+
+//Lets use some variables//
+let a = 10
+console.log(a)
+a = 20
+console.log(a)
+let b = 7 * a
+console.log(b)
+
+//Sequence of operations//
+const max = 57
+const actual = max - 13
+const percentage = actual / max
+console.log(percentage)
